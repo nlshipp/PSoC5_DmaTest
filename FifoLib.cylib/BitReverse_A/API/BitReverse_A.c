d80dbd6a637d076e379cf3c456ea42bbf48ad384
nlshipp <nlshipp@yahoo.com>
2023-07-21 17:42:14 -0700

    Add 4 bit reversal variants
     - BitReverse_A - Uses S/R flip flop to keep track of which DMA channel
       is running. ctrl_DMA and data_DMA are single TD channels.
       Uses 4 macrocells and 3 P-terms.
    
     - BitReverse_B - Same as A variant but uses toggle flip flop.
       Uses 4 macrocells and 3 P-terms.
    
     - BitReverse_C - Uses control reg written by DMA channels to keep
       track of which DMA channel is running. ctrl_DMA and data_DMA use
       2 TDs each.  One to write control/data values, and the other to
       update the control register.
       Uses 3 macrocells and 2 P-terms.
    
     - BitReverse_D - Uses single DMA channel and two control/status
       registers wired with swapped pins to reverse bits without table
       lookup. DMA drq input is level driven directly from fifo, not
       edge driven like A, B and C.
       Uses 3 macrocells and 2 P-terms, but has possibly more complex
       signal routing due to 8 added wires connecting control and status.
    
     - Simple test of BitReverse_A completed.

---
+++ FifoLib.cylib/BitReverse_A/API/BitReverse_A.c
@@ -0,0 +1,85 @@
+/* ========================================
+ *
+ * Copyright Neil Shipp, 2023
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+/* BitReverse_A
+
+   The implementation a dual fifo block where the output of fifo0 is used as the input and the input of fifo0 is 
+   used as the output, this is common to all variants. The table lookup is done using two DMA channels ctrl/data
+   which alternate using a set/reset flip flop triggered from the nrq dma outputs.
+*/
+
+#include <cytypes.h>
+#include <assert.h>
+#include "`$INSTANCE_NAME`.h"
+#include "`$INSTANCE_NAME`_DMA_Ctrl_dma.h"
+#include "`$INSTANCE_NAME`_DMA_Data_dma.h"
+#include "`$INSTANCE_NAME`_fifo_in_out_defs.h"
+
+uint8 `$INSTANCE_NAME`_dma_ctrl;
+uint8 `$INSTANCE_NAME`_dma_data;
+
+uint8 `$INSTANCE_NAME`_dma_ctrl_TD;
+uint8 `$INSTANCE_NAME`_dma_data_TD;
+
+void `$INSTANCE_NAME`_Init(const uint8 byteTable[256])
+{
+    // byteTable must be 256 byte page aligned.
+    CYASSERT(((uint32)(byteTable) & 0xFF) == 0)
+    
+    `$INSTANCE_NAME`_fifo_in_out_fifo_F0_CLEAR;
+    `$INSTANCE_NAME`_fifo_in_out_fifo_F1_CLEAR;
+    
+    `$INSTANCE_NAME`_dma_ctrl = `$INSTANCE_NAME`_DMA_Ctrl_DmaInitialize(
+                1 /* bytes/burst */, 
+                1 /* rqst/burst */, 
+                HI16(CYDEV_PERIPH_BASE), HI16(CYDEV_PERIPH_BASE));
+                                                                        
+    `$INSTANCE_NAME`_dma_data = `$INSTANCE_NAME`_DMA_Data_DmaInitialize(
+                1 /* bytes/burst */, 
+                1 /* rqst/burst */, 
+                HI16(CYDEV_SRAM_BASE), HI16(CYDEV_PERIPH_BASE));
+    
+    `$INSTANCE_NAME`_dma_data_TD = CyDmaTdAllocate();
+    `$INSTANCE_NAME`_dma_ctrl_TD = CyDmaTdAllocate();
+    
+    // The ctrl TD loads the source byte from fifo0 into the lower order byte of the source address offset of data TD
+    CyDmaTdSetConfiguration(
+                `$INSTANCE_NAME`_dma_ctrl_TD,
+                1 /* transfer count */,
+                `$INSTANCE_NAME`_dma_ctrl_TD, /* next TD - loop back*/
+                `$INSTANCE_NAME`_DMA_Ctrl__TD_TERMOUT_EN);  // enable NRQ output to run data TD
+    CyDmaTdSetAddress(
+                `$INSTANCE_NAME`_dma_ctrl_TD,
+                LO16((uint32)`$INSTANCE_NAME`_fifo_in_out_fifo_F0_PTR),
+                LO16((uint32)&DMAC_TDMEM[`$INSTANCE_NAME`_dma_data_TD].TD1[0]));
+    
+    // The data TD loads a byte from the passed byteTable indexed by the inpute byte above, and 
+    // copies it to fifo1.
+    CyDmaTdSetConfiguration(
+                `$INSTANCE_NAME`_dma_data_TD,
+                1 /* transfer count */,
+                `$INSTANCE_NAME`_dma_data_TD, /* next TD - loop back*/
+                `$INSTANCE_NAME`_DMA_Data__TD_TERMOUT_EN);  // enable NRQ output to enable ctrl TD for next byte
+    CyDmaTdSetAddress(
+                `$INSTANCE_NAME`_dma_data_TD,
+                LO16((uint32)byteTable),
+                LO16((uint32)`$INSTANCE_NAME`_fifo_in_out_fifo_F1_PTR));
+    
+    CyDmaChSetInitialTd(`$INSTANCE_NAME`_dma_ctrl, `$INSTANCE_NAME`_dma_ctrl_TD);
+    CyDmaChSetInitialTd(`$INSTANCE_NAME`_dma_data, `$INSTANCE_NAME`_dma_data_TD);
+}
+    
+void `$INSTANCE_NAME`_Enable()
+{
+    CyDmaChEnable(`$INSTANCE_NAME`_dma_data, 1);    // preserve TD values
+    CyDmaChEnable(`$INSTANCE_NAME`_dma_ctrl, 1);    // preserve TD values
+}
