f4383f8378ef67fdb99ba2210f8b8ff5e01058b0
nlshipp <nlshipp@yahoo.com>
2023-07-23 10:29:53 -0700

    BitReverse_B2 - update T flip-flop to solve double clocking issue
     - Add variant B2 which uses a D flip flop that uses it's inverse
       as input and is clocked by the OR of the ctrl and data NRQ lines.
       Effectively the same as using toggle flip flop wired to '1' and
       clocked by the OR of the NRQ lines.
    
     - Fixes issue with double clocking, but consumes one more macrocell
       than variant A instead of one less. Variant B used one less
       because the OR logic could be folded into the toggle input of
       the T-flip flop. No such folding can happen on the clock input.

---
+++ FifoLib.cylib/BitReverse_B2/API/BitReverse_B2.h
@@ -0,0 +1,25 @@
+/* ========================================
+ *
+ * Copyright Neil Shipp, 2023
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#if !defined(BitReverse_B2_`$INSTANCE_NAME`_H)
+#define BitReverse_B2_`$INSTANCE_NAME`_H
+
+#define `$INSTANCE_NAME`_fifo_in `$INSTANCE_NAME`_fifo_in_out_fifo_F0_REG
+#define `$INSTANCE_NAME`_fifo_out `$INSTANCE_NAME`_fifo_in_out_fifo_F1_REG
+
+// Basic interface
+void `$INSTANCE_NAME`_Init(const uint8 byteTable[256]);
+void `$INSTANCE_NAME`_Enable();
+void `$INSTANCE_NAME`_Disable();
+
+#endif
+
+/* [] END OF FILE */
